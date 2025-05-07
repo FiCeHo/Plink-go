5ede2a04d7e75548cbf605aaf9a539b32364ce6c
iagofigar <iago.figar@sarria.salesians.cat>
2025-05-07 17:32:59 +0200

    Spawnear mas pelotas, ajustes de resolucion y destruir las pelotas

---
+++ plink-go!/game.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var player_scene: PackedScene
+
+func _on_touch_screen_button_2_pressed():
+	# Create a new instance of the player scene.
+	var player = player_scene.instantiate()
+
+	# Set the player's position to the random location.
+	player.global_position.x = 577
+	player.global_position.y = 122
+
+	# Choose the velocity for the player.
+	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	#player.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the player by adding it to the Main scene.
+	add_child(player)
--- plink-go!/player.gd
+++ plink-go!/player.gd
@@ -1,29 +1,38 @@
-extends RigidBody2D
+extends Node2D
 
 var isSetup = true
 var initial_position: Vector2
 var fallThreshold = 700
+var limit = 4
 
 func _ready():
-	initial_position = global_position
-	
+	initial_position = get_node("RigidBody2D").global_position
+
 func _physics_process(delta):
 	if isSetup:
 		if Input.is_action_pressed("ui_touch"):
-			gravity_scale = 3.5
+			get_node("RigidBody2D").gravity_scale = 3.5
 			isSetup = false
-			angular_velocity = randf_range(-5, 5)
+			while get_node("RigidBody2D").angular_velocity == 0:
+				get_node("RigidBody2D").angular_velocity = randf_range(-5, 5)
 		else:
 			_process(delta)
 			
 func _process(delta):
-	if global_position.y > fallThreshold:
+	if get_node("RigidBody2D").global_position.y > fallThreshold:
 		respawn()
+		if limit == 0:
+			queue_free()
+		else:
+			limit -= 1
 		
 func respawn():
-	global_position = initial_position
-	gravity_scale = 0.0
-	linear_velocity = Vector2.ZERO
-	angular_velocity = 0
+	get_node("RigidBody2D").global_position = initial_position
+	get_node("RigidBody2D").gravity_scale = 0.0
+	get_node("RigidBody2D").linear_velocity = Vector2.ZERO
+
+	get_node("RigidBody2D").angular_velocity = 0
 	rotation = 0
+	#isSetup = true
+
 	isSetup = true
