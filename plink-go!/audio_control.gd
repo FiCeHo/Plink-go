f339fc30f2a5801440f576f6e8188659975991fa
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-05-14 19:02:55 +0200

    Minor fixes

--- plink-go!/audio_control.gd
+++ plink-go!/audio_control.gd
@@ -5,6 +5,8 @@ var audioBusId
 
 func _ready() -> void:
 	audioBusId = AudioServer.get_bus_index(audio_bus_name)
+	var db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(audioBusId, db)
 
 func _on_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
