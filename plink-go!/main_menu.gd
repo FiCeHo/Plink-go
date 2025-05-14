d31700735ed424bbed7c3063fad6dc61546db6e6
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-05-14 18:24:38 +0200

    Added Config

--- plink-go!/main_menu.gd
+++ plink-go!/main_menu.gd
@@ -1,13 +1,24 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
+func _ready() -> void:
+	main_buttons.visible = true
+	options.visible = false
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
 func _on_options_pressed() -> void:
-	print("Config")
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_opts_pressed() -> void:
+	_ready();
