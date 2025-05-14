09ea8fa673be2adcca8e47ef9db2152776844662
iagofigar <iago.figar@sarria.salesians.cat>
2025-05-14 17:25:34 +0200

    we

--- plink-go!/game.gd
+++ plink-go!/game.gd
@@ -2,18 +2,28 @@ extends Node
 
 @export var player_scene: PackedScene
 
+var ball_limit = 4
+var previous_player
+
 func _on_touch_screen_button_2_pressed():
-	# Create a new instance of the player scene.
-	var player = player_scene.instantiate()
+	if previous_player != null:
+		previous_player.get_node("RigidBody2D").gravity_scale = 3.5
+		while previous_player.get_node("RigidBody2D").angular_velocity == 0:
+			previous_player.get_node("RigidBody2D").angular_velocity = randf_range(-5, 5)
+	
+	if ball_limit > 0:
+		var player = player_scene.instantiate()
 
-	# Set the player's position to the random location.
-	player.global_position.x = 577
-	player.global_position.y = 122
-	player.hud = get_node("HUD")
+		player.global_position = Vector2(577, 122)
+		player.hud = get_node("HUD")
 
-	# Choose the velocity for the player.
-	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	#player.linear_velocity = velocity.rotated(direction)
+		add_child(player)
+		print(ball_limit)
+		ball_limit -= 1
+		previous_player = player
 
-	# Spawn the player by adding it to the Main scene.
-	add_child(player)
+func _on_ready() -> void:
+	previous_player = player_scene.instantiate()
+	previous_player.global_position = Vector2(577, 122)
+	previous_player.hud = get_node("HUD")
+	add_child(previous_player)
--- plink-go!/player.gd
+++ plink-go!/player.gd
@@ -3,39 +3,26 @@ extends Node2D
 @export var hud : CanvasLayer
 
 var isSetup = true
-var initial_position: Vector2
+var initial_position = Vector2(577, 122)
 var fallThreshold = 900
-var limit = 4
+var limit = 0
 
-func _ready():
-	initial_position = get_node("RigidBody2D").global_position
-
-func _physics_process(delta):
-	if isSetup:
-		if Input.is_action_pressed("ui_touch"):
-			get_node("RigidBody2D").gravity_scale = 3.5
-			isSetup = false
-			while get_node("RigidBody2D").angular_velocity == 0:
-				get_node("RigidBody2D").angular_velocity = randf_range(-5, 5)
-		else:
-			_process(delta)
-			
 func _process(delta):
 	if get_node("RigidBody2D").global_position.y > fallThreshold:
-		respawn()
 		if limit == 0:
 			queue_free()
 		else:
 			limit -= 1
+			respawn()
 		
 func respawn():
 	get_node("RigidBody2D").global_position = initial_position
 	get_node("RigidBody2D").gravity_scale = 0.0
 	get_node("RigidBody2D").linear_velocity = Vector2.ZERO
 
-	get_node("RigidBody2D").angular_velocity = 0
+	while get_node("RigidBody2D").angular_velocity == 0:
+		get_node("RigidBody2D").angular_velocity = randf_range(-5, 5)
 	rotation = 0
-	#isSetup = true
 
 	isSetup = true
 
