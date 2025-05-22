d6f29450650e754edf684db784118d627fa48094
iagofigar <iago.figar@sarria.salesians.cat>
2025-05-22 18:28:32 +0200

    something somethign

--- plink-go!/player_variables.gd
+++ plink-go!/player_variables.gd
@@ -1,7 +1,9 @@
 extends Node
 
-var ball_array : Array[PackedScene]
+var ball_array
 var perk_array = []
+var current_score = 0
+var current_round = 1
 
 func _init():
 	ball_array = Global.initial_ball_array
