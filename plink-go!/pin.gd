c82a50d072292b0234e1fc7b775016b9d12b24f7
Filip Ceremis Homitchi <filip.ceremis@gmail.com>
2025-05-26 16:58:54 +0200

    Pin sound effect

---
+++ plink-go!/pin.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var is_playing = false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if not is_playing:
+		var audio_player = $AudioStreamPlayer
+		if audio_player and not audio_player.playing:
+			audio_player.play()
+			is_playing = true
